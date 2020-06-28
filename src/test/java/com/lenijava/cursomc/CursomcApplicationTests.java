83ba2bda34540d05e5be6b1a891d36e84f81e331
Lenilson Goncalves <lenilson.ms@gmail.com>
2020-06-28 16:21:13 -0300

    Criacao do projeto

---
+++ src/test/java/com/lenijava/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenijava.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
